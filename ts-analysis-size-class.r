960e6d2d4e74c43f652cf97fe57128d7a605e82f
Juliana <mjbedoya@ufl.edu>
2015-10-27 12:25:30 -0400

    New function created for size class

--- ts-analysis-size-class.r
+++ ts-analysis-size-class.r
@@ -7,3 +7,14 @@ get_data<-function(){
   read.csv("surveys")
   return(data)
 }
+
+get_size_class <-function(weight){
+  if(weight > 50) {
+    size_class <- "large"
+  }
+  else{
+    size_class<-"small"
+  }
+  return(size_class)
+}
+# Use a threshold for determining size class
