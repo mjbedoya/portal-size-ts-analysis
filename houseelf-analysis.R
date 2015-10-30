fad0841a9a7dd67be3251e8aedc23fd5f62b2bb8
Juliana <mjbedoya@ufl.edu>
2015-10-30 13:02:40 -0400

    Change the name of the csv file and the code was updated

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -3,4 +3,6 @@
 # Study:  relationship between the length of house-elves' ears and aspects of their DNA
 
 # Importing the data
-houseelf_data<-read.csv("data/houseelf_earlength_dna_data")
+houseelf_data<-read.csv("data/houseelf_earlength_dna_data_1")
+
+
