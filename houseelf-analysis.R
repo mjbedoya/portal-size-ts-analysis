2bfd586295290ffd6b40d53ed897b2ce7a86b967
Juliana <mjbedoya@ufl.edu>
2015-10-30 12:45:57 -0400

    Importing data set called: houseelf_earlength_dna_data

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,1 +1,6 @@
 ## Continuing the analysis of house-elves (Dr. Granger)
+
+# Study:  relationship between the length of house-elves' ears and aspects of their DNA
+
+# Importing the data
+houseelf_data<-read.csv("data/houseelf_earlength_dna_data")
