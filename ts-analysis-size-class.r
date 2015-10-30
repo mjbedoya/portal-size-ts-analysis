7df4990b2f140472fcfddb147b7ace39f6e043f3
Juliana <mjbedoya@ufl.edu>
2015-10-30 12:42:24 -0400

    New function to add classes

--- ts-analysis-size-class.r
+++ ts-analysis-size-class.r
@@ -3,8 +3,10 @@
 # Under the git tab at the right:
 # git ignore: just when I dint want to save or ignore something
 
+
+library(dplayer)
 get_data<-function(){
-  read.csv("surveys")
+  data<-read.csv("surveys")
   return(data)
 }
 
@@ -16,5 +18,14 @@ get_size_class <-function(weight, threshold){
     size_class<-"small"
   }
   return(size_class)
+}
+
+add_size_class<- function(df){
+  data_w_size_class <-
+    df %>%
+    na.omit() %>%
+    rowwise() %>%
+    mutate(size_calss=get_size_calss(weight, 50))
+  return(data_w_size_class)
 }
 
