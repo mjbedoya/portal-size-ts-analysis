f89408df0efb23a0509a896f4eed9792832893db
Juliana <mjbedoya@ufl.edu>
2015-10-30 13:35:23 -0400

    Modification of the function with str_to_upper

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -5,28 +5,21 @@
 # Importing the data
 houseelf_data<-read.csv("houseelf_earlength_dna_data_2.csv")  ; head(houseelf_data)
 
-
-
-# Ccalculation of the GC-content of a sequence
+# Calculation of the GC-content of a sequence
 # Using the function str_to_lower or str_to_upper in the stringr package might be useful). 
 
 library(stringr)
 
 read_dna_gc <- function (data) {
+  data<-str_to_upper(data)
   Gs <- str_count(data, 'G')
   Cs <- str_count(data, 'C')
   gc_content <- (Gs + Cs) / str_length(data) * 100
   return(gc_content)
 }
 
-gccontent=read_dna_gc(houseelf_data$dnaseq)     # Call the content
+gccontent=read_dna_gc(houseelf_data$dnaseq)     # Call the content (take a vector of sequences and return a vector of GC-contents)
 
 
 
-#This function should also be able to take a vector of sequences and return a vector of GC-contents 
-#(it probably does this without any extra work so give it a try).
 
-Commit this change.
-Once you've committed the change click the Push button in the upper right corner of the window and then click OK when git is done pushing.
-You should be able to see the changes you made on Github.
-Email your teacher to let them know that you've finished this exercise.
